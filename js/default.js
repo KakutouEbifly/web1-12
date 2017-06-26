664908cf4ec7c54f0793de871e4451910af204b2
KakutouEbifly <0ty3263000b474x@ezweb.ne.jp>
2017-06-26 18:08:14 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,15 +1,34 @@
 
 for(var i = 1; i <= 100; i++){
   var number = i;
+  var decision3 = number, Decision5;
+  var flag3 = 0, flag5 = 0;
   var message;
   
+  //条件内の数字かどうか判断
+  //3の倍数？
+  do{
+    dicision /= 3;
+    if(dicsion === 1){
+      flag3 = 1;
+    }
+  }while(dicision <= 1)
+  //5の倍数？
+  do{
+    dicision = number;
+    dicision /= 5;
+    if(dicsion === 1){
+      flag5 = 1;
+    }
+  }while(dicsion <= 1);
+  
   //条件内の倍数であったときの処理
-  if(number / 3 === 1 || number / 5 === 1){
+  if(flag3 === 1 || flag5 === 1){
     
     //5の倍数
-    if(number / 3 !== 1){
+    if(flag3 !== 1){
       message = 'Buzz';
-    }else if(number / 5 !== 1){
+    }else if(flag5 !== 1){
       //3の倍数
       message = 'Fizz';
     }else{
