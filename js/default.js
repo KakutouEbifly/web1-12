dac729577949aea77b298bdae3f0c21b2da3372f
KakutouEbifly <0ty3263000b474x@ezweb.ne.jp>
2017-06-26 17:28:34 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,1 +1,25 @@
 
+for(var i = 1; i <= 100; i + 1){
+  var number = i;
+  var message;
+  
+  //条件内の倍数であったときの処理
+  if(number / 3 === 1 || number / 5 === 1){
+    
+    //5の倍数
+    if(number / 3 !== 1){
+      message = 'Buzz';
+    }else if(number / 5 !== 1){
+      //3の倍数
+      message = 'Fizz';
+    }else{
+      //15の倍数
+      message = 'Fizz Buzz';
+    }
+  }else{
+    //条件内の倍数でなかった場合の処理
+    message = number;
+  }
+  
+  document.getElementById('FizzBuzz').teztContent = 'Number:' + message;
+}
